763b0979fafbd47efa5bddba974791c1631512eb
Kunal Sarpal <115573463+Kunal-Sarpal@users.noreply.github.com>
2024-07-22 02:13:41 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -11,13 +11,7 @@ interface config{
     databaseURL: string;
 }
 const firebaseConfig:config = {
-    apiKey: "AIzaSyCSorESWxd1M9bZOPf3JunxwmihhOuJKyI",
-    authDomain: "kunal-app-14939.firebaseapp.com",
-    projectId: "kunal-app-14939",
-    storageBucket: "kunal-app-14939.appspot.com",
-    messagingSenderId: "94453707364",
-    appId: "1:94453707364:web:4c0196d030d1403bb2e571",
-    databaseURL: "https://kunal-app-14939-default-rtdb.firebaseio.com/"
+ // .....credentials
 };
 
 
